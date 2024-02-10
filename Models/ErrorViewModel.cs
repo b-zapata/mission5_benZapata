57e5f20ccc4ce5414d55e191bfa0f5a46193fe3a
b-zapata <150874080+b-zapata@users.noreply.github.com>
2024-02-09 23:15:01 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mission5_benZapata.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$("#btnCalculate").click( function () {
+    let hoursDesired = $("#hoursDesired").val();
+    let hourlyRate = $("#hourlyRate").val();
+    let totalCost = hoursDesired * hourlyRate;
+    console.log("Total cost: " + totalCost);
+    $("#totalCost").val(totalCost);
+})
